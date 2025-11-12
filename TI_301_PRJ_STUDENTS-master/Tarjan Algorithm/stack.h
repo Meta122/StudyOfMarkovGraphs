21eddfb36b4f52f8ad4b11af6379dd26a254bc7c
Metacraft <rafy.vec@gmail.com>
2025-11-12 09:13:57 +0100

    Added isInStack, converted stack type to int

--- TI_301_PRJ_STUDENTS-master/Tarjan Algorithm/stack.h
+++ TI_301_PRJ_STUDENTS-master/Tarjan Algorithm/stack.h
@@ -3,7 +3,7 @@
 #include "tarjan.h"
 
 typedef struct s_stackcell {
-  t_tarjan_vertex vertex ;
+  int number ;
   struct s_stackcell * next ;
 } t_stackcell ;
 
@@ -11,7 +11,8 @@ typedef struct s_stacklist {
   t_stackcell * head ;
 } t_stacklist ;
 
-t_tarjan_vertex pop(t_stacklist *) ;
-t_tarjan_vertex top(t_stacklist) ;
+int pop(t_stacklist *) ;
+int top(t_stacklist) ;
+int isInStack(int, t_stacklist) ;
 
 #endif //STACK_H
