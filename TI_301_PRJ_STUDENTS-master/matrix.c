1864d32d4d8d78b99217c6800d12c1451d77f52d
Sky <frederic.pacreau@efrei.net>
2025-11-20 15:43:57 +0100

    functions copy and create matrix

---
+++ TI_301_PRJ_STUDENTS-master/matrix.c
@@ -0,0 +1,38 @@
+//
+// Created by overl on 20/11/2025.
+//
+
+float * create_matrix_zeros(int n)
+{
+    float * lines[n];
+    for (int i = 0; i < n; i++)
+    {
+        float column[n];
+        for (int j = 0; j < n; j++)
+        {
+            column[j] = 0;
+        }
+        lines[i] = column;
+    }
+    return lines;
+}
+
+float * copy_matrix(float * matr)
+{
+    int lenght=0;
+    while (matr[lenght])
+    {
+        lenght++;
+    }
+    float * mcopy[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        for (int j = 0; j < lenght; j++)
+        {
+            mcopy[lenght] = matr[lenght];
+        }
+    }
+    return mcopy;
+
+}
+
---
+++ TI_301_PRJ_STUDENTS-master/matrix.h
@@ -0,0 +1,14 @@
+//
+// Created by overl on 20/11/2025.
+//
+
+#ifndef STUDYOFMARKOVGRAPHS_MATRIX_H
+#define STUDYOFMARKOVGRAPHS_MATRIX_H
+
+float * create_matrix_zeros(int n);
+
+float * copy_matrix(float * matr);
+
+
+
+#endif //STUDYOFMARKOVGRAPHS_MATRIX_H
